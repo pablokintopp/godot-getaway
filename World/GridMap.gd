3d2aaa4802e2f842b2b98001c813a69e9bd5c0dd
= <=>
2019-11-07 11:10:56 -0300

    Fix walls generation on map

--- World/GridMap.gd
+++ World/GridMap.gd
@@ -7,7 +7,9 @@ const W = 8
 
 var width = 20
 var height = 20
-var spacing = 3
+var spacing = 2
+
+var erase_fraction = 0.25
 
 var cell_walls = {Vector3(0, 0, -spacing): N, Vector3(spacing, 0, 0): E, 
 		Vector3(0, 0, spacing): S, Vector3(-spacing, 0, 0): W }
@@ -20,6 +22,7 @@ func _ready():
 func make_map():
 	make_blank_map()
 	make_maze()
+	erase_walls()
 
 func make_blank_map():
 	for x in width:
@@ -78,6 +81,28 @@ func fill_gaps(current, dir):
 			tile_type = 10
 			current.z -= 1
 		set_cell_item(current.x, 0, current.z, tile_type, 0)
+		
+		
+func erase_walls():
+	for i in range(width * height * erase_fraction):
+		var x =  int(rand_range(1, (width - 1)/spacing)) * spacing
+		var z =  int(rand_range(1, (height - 1)/spacing)) * spacing
+		var cell = Vector3(x, 0, z)
+		var current_cell = get_cell_item(cell.x, cell.y, cell.z)
+		var neighbour = cell_walls.keys()[randi() % cell_walls.size()]
+		
+		if current_cell & cell_walls[neighbour]:
+			var walls = current_cell - cell_walls[neighbour]
+			var neighbout_walls = get_cell_item(cell.x + neighbour.x, 0, cell.z + neighbour.z) - cell_walls[-neighbour]
+			
+			set_cell_item(cell.x, 0, cell.z, 0)
+			set_cell_item( (cell + neighbour).x, 0, (cell + neighbour).z, neighbout_walls, 0)
+			fill_gaps(cell, neighbour)
+	
+	
+	
+	
+	
 		
 		
 	
